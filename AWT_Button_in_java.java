26987b10f2a0f269002f05db3d99b75991a790eb
Darshan Soni <sonidarshan200@gmail.com>
2023-04-19 18:40:56 +0530

    some changes

--- AWT_Button_in_java.java
+++ AWT_Button_in_java.java
@@ -1,22 +1,18 @@
-import java.awt.*;  
+import java.awt.*;
 
+public class AWT_Button_in_java {
+    public AWT_Button_in_java() {
+        Frame f = new Frame();
+        Button btn = new Button("Hello World");
+        btn.setBounds(80, 80, 100, 50);
+        f.add(btn);
+        f.setSize(300, 250);
+        f.setTitle("Sejal_Shinde_2213310");
+        f.setLayout(null);
+        f.setVisible(true);
+    }
 
-public class AWTProgram 
-{  
-public AWTProgram()  
-    {  
-        Frame f = new Frame();  
-        Button btn=new Button("Hello World");  
-        btn.setBounds(80, 80, 100, 50);  
-        f.add(btn);          
-        f.setSize(300, 250);          
-        f.setTitle("Sejal_Shinde_2213310");    
-        f.setLayout(null);     
-        f.setVisible(true);            
-    }  
-  
- public static void main(String[] args) 
- {
-	 AWTProgram awt = new AWTProgram();
- }
+    public static void main(String[] args) {
+        AWT_Button_in_java awt = new AWT_Button_in_java();
+    }
 }
